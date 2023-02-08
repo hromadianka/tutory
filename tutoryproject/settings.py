48a1f88eb915777fb76e89be42c43eb8d705a188
aspirati0 <99715792+aspirati0@users.noreply.github.com>
2023-02-08 20:55:17 +0300

    Update settings.py

--- tutoryproject/settings.py
+++ tutoryproject/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 import whitenoise
+import psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
