af02d8ab689868b8c40f940f660e63314cc2a02a
aspirati0 <99715792+aspirati0@users.noreply.github.com>
2023-02-16 03:01:30 +0300

    Update settings.py

--- tutoryproject/settings.py
+++ tutoryproject/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+django_heroku.settings(locals())
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
