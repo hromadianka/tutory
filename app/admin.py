f4151b1e67d9b46e062b045cd369c031c56d3bb1
aspirati0 <99715792+aspirati0@users.noreply.github.com>
2023-02-16 16:39:55 +0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import AddProject, Profile, Project, Result, Feedback
+from .models import AddProject, Profile, Project, Result, Feedback, CourseProject
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(Project)
 admin.site.register(AddProject)
 admin.site.register(Result)
 admin.site.register(Feedback)
+admin.site.register(CourseProject)
